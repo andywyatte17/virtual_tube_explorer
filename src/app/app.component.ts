06a36c1294790cf32cde265a0ca1bb6453d7887b
Andrew Wyatt <andyw@emnetsoftware.com>
2018-05-03 09:27:39 +0100

    Encryption experiment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+import { LOCATION_INITIALIZED } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  // ...
+
+  ngOnInit()
+  {
+    this.updateThingEncrypted();
+  }
+
+  thingToEncrypt = '{ "TflApiKey": "...", "TflApiSecret": "..." }';
+  
+  thingEncrypted = '???';
+
+  thingToEncryptDidChange(value : string) {
+    this.thingToEncrypt = value;
+    this.updateThingEncrypted();
+  }
+
+  private updateThingEncrypted()
+  {
+  this.thingEncrypted = CryptoJS.HmacSHA256(this.thingToEncrypt, "someKey").toString();
+  }
 }
